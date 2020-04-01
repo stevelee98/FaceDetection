dcacc15806ecf9b9ff784435f82d979a2bbe0d6a
hoangvietle2598 <viet.lh.2598@gmail.com>
2020-04-01 10:06:06 +0700

    - PUSH FILE ANDROID

---
+++ android/app/src/main/java/com/facerecognize/MainActivity.java
@@ -0,0 +1,34 @@
+package com.facerecognize;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+
+import org.devio.rn.splashscreen.SplashScreen;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "thoitrangaman";
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ android/app/src/main/java/com/facerecognize/splash/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.facerecognize.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facerecognize.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        // Pass along FCM messages/notifications etc.
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            intent.putExtras(extras);
+        }
+        startActivity(intent);
+        finish();
+    }
+}
